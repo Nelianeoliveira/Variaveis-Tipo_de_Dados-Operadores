eb7d14562cf599250240fcf7c33218a490e07b30
Neliane de Oliveira <92684306+Nelianeoliveira@users.noreply.github.com>
2022-07-20 21:42:41 -0300

    Initial commit

---
+++ src/br/com/operadores/tipo_de_dados/variaveis/Aula01/Main.java
@@ -0,0 +1,49 @@
+package br.com.operadores.tipo_de_dados.variaveis.Aula01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+    {
+
+        int i;
+        //int 1;
+        int I;
+        //int 1a;
+        int _1a;
+        int $aq;
+
+        i =5;
+        I =10;
+        _1a =20;
+        $aq =7;
+
+        final int j = 10;
+        //j = 15;
+        int asre2467bad = 3;
+        int asrn27678_ad = 100;
+//int asrn27678%_ad = 10;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto = 5;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+//INT qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asre2467bad);
+        System.out.println(asrn27678_ad);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+    }
+}
+
